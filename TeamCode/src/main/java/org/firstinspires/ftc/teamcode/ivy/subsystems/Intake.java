aefd4f2461b22376e0d8592b58c7c59343bfd0bd
Malav <thisismalav@gmail.com>
2026-08-07 10:42:01 -0500

    Mecanum+Init done in TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ivy/subsystems/Intake.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.ivy.subsystems;
+
+import static com.pedropathing.ivy.commands.Commands.instant;
+
+import com.pedropathing.ivy.Command;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake {
+
+    public enum State {INTAKE, OUTTAKE, IDLE};
+
+    private final DcMotorEx intake;
+    private final DcMotorEx index;
+    private State state = State.IDLE;
+    public Intake(HardwareMap hardwareMap){
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+        intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        intake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intake.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        index = hardwareMap.get(DcMotorEx.class,"index");
+        index.setDirection(DcMotorSimple.Direction.FORWARD);
+        index.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        index.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void setState (State newState){
+        state = newState;
+        switch (newState){
+            case INTAKE:
+                intake.setPower(1.0);
+                index.setPower(1.0);
+                break;
+            case OUTTAKE:
+                intake.setPower(-1.0);
+                index.setPower(-1.0);
+                break;
+            case IDLE:
+                intake.setPower(0);
+                index.setPower(0);
+                break;
+        }
+
+    }
+
+    public Command in(){
+        return instant(() -> setState(State.INTAKE)).requiring(this);
+    }
+
+    public Command out(){
+        return instant(() -> setState(State.OUTTAKE)).requiring(this);
+    }
+    public Command idle(){
+        return instant(() -> setState(State.IDLE)).requiring(this);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.ivy.subsystems.Intake;
+
+@TeleOp
+public class teleop extends OpMode {
+    private Intake intake;
+    private Intake index;
+    DcMotor leftFront, rightFront, leftBack, rightBack;
+    public double SpeedMultiplier = 0.65;
+
+    @Override
+    public void init() {
+        /** Mec Init **/
+        leftFront=hardwareMap.get(DcMotor.class, "fl");
+        rightFront=hardwareMap.get(DcMotor.class,"fr");
+        leftBack=hardwareMap.get(DcMotor.class,"bl");
+        rightBack=hardwareMap.get(DcMotor.class,"br");
+
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+        /** Intake Init **/
+        intake = new Intake(hardwareMap);
+        index = new Intake(hardwareMap);
+
+    }
+
+    @Override
+    public void loop() {
+
+        /** ---------------- Mecanum ---------------- **/
+        double drive = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+
+        double leftFrontPower = drive+strafe+turn;
+        double rightFrontPower= drive-strafe-turn;
+        double leftBackPower = drive-strafe+turn;
+        double rightBackPower = drive+strafe-turn;
+
+        double max = Math.max(
+                Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower)),
+                Math.max(Math.abs(leftBackPower), Math.abs(rightBackPower))
+        );
+
+        if(max > 1.0){
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        leftFront.setPower(leftFrontPower*SpeedMultiplier);
+        rightFront.setPower(rightFrontPower*SpeedMultiplier);
+        leftBack.setPower(leftBackPower*SpeedMultiplier);
+        rightBack.setPower(rightBackPower*SpeedMultiplier);
+
+
+        /** ---------------- Intake ---------------- **/
+        if(gamepad1.left_bumper){
+            intake.in();
+        } else if (gamepad1.right_bumper) {
+            intake.idle();
+        } else if (gamepad1.left_bumper && gamepad1.right_bumper) {
+            intake.out();
+        }
+
+    }
+}

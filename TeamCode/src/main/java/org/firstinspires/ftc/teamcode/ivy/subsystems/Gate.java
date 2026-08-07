651633f6a89457996a90af0961833ae443524cd0
Tarun R <158979401+Ninja10oo@users.noreply.github.com>
2026-08-07 11:17:26 -0500

    Created Gate (fix command error).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ivy/subsystems/Gate.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.ivy.subsystems;
+
+import static com.pedropathing.ivy.commands.Commands.instant;
+
+import com.pedropathing.ivy.Command;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Gate {
+
+    public enum State {CLOSE, OPEN};
+    private final Servo gate;
+    private State state = State.CLOSE;
+    public Gate(HardwareMap hardwareMap){
+        gate = hardwareMap.get(Servo.class, "gate");
+    }
+
+
+
+    public void setState (State newState){
+        state = newState;
+        switch (newState){
+            case CLOSE:
+                gate.setPosition(0);
+                break;
+            case OPEN:
+                gate.setPosition(0.5);
+        }
+
+    }
+
+    public Command cl() { return instant(() -> setState(State.CLOSE).requiring(this); }
+
+    public Command op(){
+        return instant(() -> setState(State.OPEN)).requiring(this);
+    }
+}
